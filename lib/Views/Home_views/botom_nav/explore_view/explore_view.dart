30836f43902e00d2ebd8b3457147b45951321b9b
rehman <rehmankh2256@gmail.com>
2025-04-26 15:00:34 +0500

    onboarding

--- lib/Views/Home_views/botom_nav/explore_view/explore_view.dart
+++ lib/Views/Home_views/botom_nav/explore_view/explore_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:online_groceries_app/controller/constants/appasset/appasset.dart';
 class ExploreView extends StatefulWidget {
   const ExploreView({super.key});
 
@@ -9,6 +10,38 @@ class ExploreView extends StatefulWidget {
 class _ExploreViewState extends State<ExploreView> {
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text("Explore View"),);
+    return Scaffold(
+      body: Column(
+        children: [
+          Expanded(child: Container(color: Colors.grey,)),
+          Expanded(
+            child: PageView( children: [
+              ExploreScreen(img: Appassets.appleImage,text: "Apple"),
+              ExploreScreen(img: Appassets.bananaImage,text: "Banana"),
+              ExploreScreen(img: Appassets.beefImage,text: "Beef Bone"),
+            ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
+class ExploreScreen extends StatelessWidget {
+  String text;
+  String img;
+  ExploreScreen({super.key, required this.text, required this.img});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Column(
+      children: [
+        Image(image: AssetImage(img)),
+        SizedBox(height: 10,),
+        Text(text),
+      ],
+    ),);
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'package:online_groceries_app/Views/Auth_views/Signup_screen/signup_scree
 import 'package:online_groceries_app/Views/Auth_views/Verification_screen/Verification_Screen.dart';
 import 'package:online_groceries_app/Views/Home_views/HomeScreen.dart';
 import 'package:online_groceries_app/Views/Home_views/botom_nav/bottom_nav.dart';
+import 'package:online_groceries_app/Views/Home_views/botom_nav/explore_view/explore_view.dart';
 import 'package:online_groceries_app/Views/Starting_screen/Onboarding_screen/onboarding.dart';
 import 'package:online_groceries_app/Views/Starting_screen/Splash_screen/splash_Screen.dart';
 import 'package:online_groceries_app/Views/Auth_views/Login_screen/Login_Screen.dart';
@@ -22,7 +23,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginScreen(),
+      home: ExploreView(),
     );
   }
 }
