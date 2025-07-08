e9bd3b75691ff0e0d7bdd04396ab3ed58d7afdba
REHMAN KHAN <rehmankh2256@gmail.com>
2025-07-08 12:03:58 +0500

    routes

---
+++ lib/controller/utills/approutes/approutes.dart
@@ -0,0 +1,6 @@
+class AppRoutes{
+  static final String SplashScreen = "/";
+  static final String LoginScreen = "/LoginScreen";
+  static final String SigninScreen = "/SigninScreen";
+  static final String SignupnScreen = "/SignupScreen";
+}
