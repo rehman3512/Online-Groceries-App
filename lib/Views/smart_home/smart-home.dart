bde1362275f2afc63cd3b997f059df131bb1974b
REHMAN KHAN <rehmankh2256@gmail.com>
2025-07-24 09:31:22 +0500

    smart-home

---
+++ lib/Views/smart_home/smart-home.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SmartHome extends StatefulWidget {
+  const SmartHome({super.key});
+
+  @override
+  State<SmartHome> createState() => _SmartHomeState();
+}
+
+class _SmartHomeState extends State<SmartHome> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
